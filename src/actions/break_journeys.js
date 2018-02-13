3b1d904b9ead00520f35d43f5859c16d6ecadd56
Santanu Bhattacharya <sbhattacharya@kreeti.com>
2018-02-13 18:29:12 +0530

    trying with api

--- src/actions/break_journeys.js
+++ src/actions/break_journeys.js
@@ -1,11 +1,15 @@
 import { createAction } from 'redux-actions';
 import { FETCH_BREAK_JOURNEYS } from '../constants/actionTypes';
+import { fetch_data } from '../api/break_journeys';
 
 const loadBJSuccess = createAction(FETCH_BREAK_JOURNEYS);
 
 export function loadBJs() {
     return(dispatch) => {
-        dispatch(loadBJSuccess);
+      return fetch_data()
+        .then(payload => {
+          dispatch(loadBJSuccess(payload))
+        });
     }
 }
 
