04d82480d320b5d70abf47e6bd5c29747b385d0d
Santanu Bhattacharya <sbhattacharya@kreeti.com>
2018-02-19 18:50:11 +0530

    google api used

--- src/Trip/ShowDistance.js
+++ src/Trip/ShowDistance.js
@@ -3,12 +3,30 @@ import { Container, Header, Input, Left, Body, Title, Card, CardItem,
     Content, Right, Icon, Button, Text } from "native-base";
 
     class ShowDistance extends React.Component {
-        constructor(props) {
-          super(props);
-        }
+      constructor(props) {
+        super(props);
+        
+        this.state = {
+          distance: 0
+        };
+      }
+      
+      componentWillMount() {
+        this.calculateDistance(this.props.lat, this.props.long, "K");
+      }
+      
+      calculateDistance(lat1, lon1, unit) {
+        const destinationLat = 21.661521;
+        const destinationLong = 87.697736;
 
-        calculateDistance(lat1, lon1, lat2, lon2, unit) {
-      			if(lat1) {
+          if(true) {
+            distanceUrl = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=22.5727259,88.4315008&destinations="+ destinationLat +","+ destinationLong+"&key=AIzaSyCRvKAFoW1sqdCsmP6G5Ss9a7pubqKgbug";
+            fetch(distanceUrl).then((data) => {
+              var response = JSON.parse(data._bodyText);
+              this.setState({ distance: response.rows[0].elements[0].distance.value/ 1000});
+            })
+          }
+      		else if(lat1 && false) {
 							var radlat1 = Math.PI * lat1/180;
 							var radlat2 = Math.PI * lat2/180;
             	var theta = lon1 - lon2;
@@ -20,7 +38,7 @@ import { Container, Header, Input, Left, Body, Title, Card, CardItem,
 
 							if(unit==="K") { dist = dist * 1.609344 }
 							if (unit=="N") { dist = dist * 0.8684 }
-							return dist;
+						  this.setState({ distance: dist });
 						} else {
 							return "still calculating";
 						}
@@ -33,7 +51,7 @@ import { Container, Header, Input, Left, Body, Title, Card, CardItem,
             return(
                 <Card>
                   <Text>The remaining distance is</Text>
-                  <Text>{this.calculateDistance(this.props.lat, this.props.long, destinationLat, destinationLong, "K")}</Text>
+                  <Text>{this.state.distance}</Text>
                 </Card>
             );
 				}
